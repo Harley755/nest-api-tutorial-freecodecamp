f2cce6a8290fd7d1513a51ab1d2f89de0ba06196
Harley <bricegoudalo@gmail.com>
2024-12-18 11:28:04 +0100

    freecodecamp nest tutorial Create a REST API

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,84 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from 'src/auth/dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+
+  async signup(dto: AuthDto) {
+    // Generate password hash
+    const hash = await argon.hash(dto.password);
+
+    // Save new User in the DB
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials taked');
+      }
+      throw error;
+    }
+
+    // Return the user
+  }
+
+  async login(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // throw exception if user doesn't exist
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    // compare passsword
+    const pwdMatch = await argon.verify(user.hash, dto.password);
+    // throw exception if password doesn't match
+    if (!pwdMatch) throw new ForbiddenException('Credentials incorrect');
+
+    // send back the user
+    // delete user.hash;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,3 @@
+describe('App e2e', () => {
+  it.todo('Should pass');
+});
